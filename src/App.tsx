e54abd968dcc14d5681c25638eb9efa63dd08176
Baltasar Klix Jones <balta98_@hotmail.com>
2023-07-18 17:42:09 -0300

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Container } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <Container maxWidth="xl">
+        <Button variant='contained'> 
+          Hola mundo
+        </Button>
+    </Container>
+  )
+}
+
+export default App
