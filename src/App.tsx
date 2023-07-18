552902919c93ea673696e8d1173ead74bbbcf9c8
Baltasar Klix Jones <balta98_@hotmail.com>
2023-07-18 18:24:12 -0300

    NavBar y routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Button, Container } from '@mui/material'
+
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { AppRouter } from './Router'
 
 function App() {
 
   return (
-    <Container maxWidth="xl">
-        <Button variant='contained'> 
-          Hola mundo
-        </Button>
-    </Container>
+    <div>
+      <BrowserRouter>
+      <AppRouter/>
+      </BrowserRouter>
+     
+    </div>
   )
 }
 
