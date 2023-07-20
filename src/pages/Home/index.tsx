3b610934e5932b32d4dc600131363512d10cbf89
Baltasar Klix Jones <balta98_@hotmail.com>
2023-07-20 09:44:24 -0300

    llamado a la api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Button, Container } from "@mui/material";
 import { HeaderComponent } from "../../components";
+import { characters } from "../../api/characters";
 
 export const HomePage: React.FC<{}> = () => {
+
+
+  React.useEffect(() => {
+    characters.getById({id: 1}).then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  },[])
+
   return (
     <Container maxWidth="xl">
-      <HeaderComponent title="Hola mundo" description="Hola mundo bienvenido" element={<Button fullWidth variant="contained">Hola mundo</Button>} />
+      <HeaderComponent
+        title="Hola mundo"
+        description="Hola mundo bienvenido"
+        element={
+          <Button fullWidth variant="contained">
+            Hola mundo
+          </Button>
+        }
+      />
     </Container>
   );
 };
